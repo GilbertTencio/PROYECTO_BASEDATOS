cc360abc88f5aea77cf3c245ef9fc078ca733768
Santiliwis <Santiagocorreafallas@gmail.com>
2025-07-13 19:57:12 -0600

    Update viewsList.sql
    ultimas 5 vistas

--- TECHEXPRESS/TechExpressDataBaseScripts/viewsList.sql
+++ TECHEXPRESS/TechExpressDataBaseScripts/viewsList.sql
@@ -53,3 +53,54 @@ JOIN Servicio_Cliente sc ON p.Servicio_Realizado = sc.Servicio_Realizado
 JOIN Clientes c ON sc.Cliente_ID = c.Cliente_ID
 JOIN Gestion_Servicios gs ON sc.Servicio_ID = gs.Servicio_ID
 WHERE p.Estado_ID IN (SELECT Estado_ID FROM Estado WHERE Nombre_Estado = 'Pagado');
+
+--6 Vista de clientes activos
+CREATE VIEW V_CLIENTES_ACTIVOS AS
+SELECT 
+    Cliente_ID,
+    Nombre_Cliente,
+    Apellido_Cliente,
+    Email_Cliente
+FROM Clientes
+WHERE Estado_ID = 1;
+
+--7 filtro de productos más caros
+CREATE VIEW VW_INVENTARIO_PRECIO_ALTO AS
+SELECT 
+    Item_ID,
+    Nombre_Item,
+    Precio_Unitario,
+    Cantidad_Item
+FROM Inventario
+WHERE Precio_Unitario > 100 000;
+
+-- 8 Vista de servicios realizados por fecha
+CREATE VIEW VW_SERVICIOS_REALIZADOS_FECHA AS
+SELECT 
+    Servicio_Realizado,
+    Empleado_ID,
+    Cliente_ID,
+    Fecha_ID,
+    Precio_Total
+FROM Servicio_Cliente
+WHERE Fecha_ID = TO_DATE('2025-07-01', 'YYYY-MM-DD');
+
+--9 Vista de empleados por puesto 
+CREATE VIEW VW_EMPLEADOS_TECNICOS AS
+SELECT 
+    Empleado_ID,
+    Nombre_Empleado,
+    Apellido_Empleado,
+    Puesto
+FROM Empleados
+WHERE Puesto = 'Tecnico';
+
+-- 10 Vista de pagos en efectivo
+CREATE VIEW VW_PAGOS_EFECTIVO AS
+SELECT 
+    Pago_ID,
+    Servicio_Realizado,
+    Precio_Total,
+    Metodo_Pago
+FROM Pagos
+WHERE Metodo_Pago = 'EFECT';
